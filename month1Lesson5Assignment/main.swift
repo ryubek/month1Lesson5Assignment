45aca9f90d05d92dfeed7aa5fa1d9b3b3b67f6a9
ryubek <ryubek91@gmail.com>
2022-01-17 11:56:28 +0600

    Разобрался с кометами

--- month1Lesson5Assignment/main.swift
+++ month1Lesson5Assignment/main.swift
@@ -7,5 +7,45 @@
 
 import Foundation
 
-print("Hello, World!")
+//@frozen struct Dictionary<Key, Value> where Key : Hashable
+
+
+
+//var words: [String] = ["lemon", "orange", "grapefruit"]
+//var slova: [String] = ["лимон", "апельсин", "грейпфрут"]
+//
+//var translate = (readLine())
+//
+//func translation(){
+//    for slova in 0..<words.count{
+//        print(words[slova])
+//    }
+//}
+//
+//translation()
+
+var baseballTerms: [String : String] = [
+    "bat" : "бита",
+    "coach" : "тренер",
+    "batter" : "отбивающий",
+    "pitcher" : "подающий",
+    "baseman" : "базовый",
+    "curveball" : "крученый мяц",
+    "inning" : "иннинг",
+]
+
+print("Введите слово для перевода")
+var word = readLine()
+
+for (key, value) in baseballTerms {
+    if word == key {
+        print("Переводом является \(value)")
+    }else {
+        print("Данное слово находится вне базы данных")
+        break
+    }
+}
+
+
+
 
